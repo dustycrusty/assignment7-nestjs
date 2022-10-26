f950052fc63e4e25fd916ce67fbc5f85de46bbbe
Dustin <sylee970711@gmail.com>
2022-10-25 19:13:26 -0700

    finish unit tests for podcasts

--- src/podcast/podcasts.service.spec.ts
+++ src/podcast/podcasts.service.spec.ts
@@ -189,6 +189,19 @@ describe('PodcastService', () => {
         error: 'ERRED',
       });
     });
+    it('should succeed if payload is null', async () => {
+      jest.spyOn(service, 'getPodcast').mockResolvedValue({
+        ok: true,
+        podcast: expect.any(Podcast),
+      });
+      const result = await service.updatePodcast({
+        ...trueUpdatePodcastArgs,
+        payload: { rating: null },
+      });
+      expect(result).toEqual({
+        ok: true,
+      });
+    });
     it('should fail when rating is not in 1~5', async () => {
       jest.spyOn(service, 'getPodcast').mockResolvedValue({
         ok: true,
@@ -212,9 +225,228 @@ describe('PodcastService', () => {
     });
   });
 
-  it.todo('getEpisodes');
-  it.todo('getEpisode');
-  it.todo('createEpisode');
-  it.todo('deleteEpisode');
-  it.todo('updateEpisode');
+  describe('getEpisodes', () => {
+    const getEpisodesArg = {
+      id: 1,
+    };
+    it('should fail with error', async () => {
+      jest.spyOn(service, 'getPodcast').mockRejectedValue(new Error());
+      const result = await service.getEpisodes(getEpisodesArg.id);
+      expect(result).toEqual(InternalServerErrorOutput);
+    });
+    it('should fail when getPodcast fails', async () => {
+      jest.spyOn(service, 'getPodcast').mockResolvedValue({
+        podcast: null,
+        ok: false,
+        error: 'erred',
+      });
+      const result = await service.getEpisodes(getEpisodesArg.id);
+      expect(result).toEqual({
+        ok: false,
+        error: 'erred',
+      });
+    });
+    it('should succeed', async () => {
+      const testPodcast = {
+        podcast: expect.any(Podcast),
+        ok: true,
+        error: null,
+      };
+      jest.spyOn(service, 'getPodcast').mockResolvedValue(testPodcast);
+      const result = await service.getEpisodes(getEpisodesArg.id);
+      console.log(result.episodes);
+      expect(result).toHaveProperty('ok', true);
+      expect(result).toHaveProperty('episodes');
+    });
+  });
+  describe('getEpisode', () => {
+    const getEpisodeArg = {
+      podcastId: 1,
+      episodeId: 2,
+    };
+    it('should fail with error', async () => {
+      jest.spyOn(service, 'getEpisodes').mockRejectedValue(new Error());
+      const result = await service.getEpisode(getEpisodeArg);
+      expect(result).toEqual(InternalServerErrorOutput);
+    });
+    it('should fail when getEpisodes fails', async () => {
+      jest.spyOn(service, 'getEpisodes').mockResolvedValue({
+        episodes: null,
+        ok: false,
+        error: 'erred',
+      });
+      const result = await service.getEpisode(getEpisodeArg);
+      expect(result).toEqual({
+        ok: false,
+        error: 'erred',
+      });
+    });
+    it('should succeed', async () => {
+      const testEpisode: Episode = {
+        id: 2,
+        title: '',
+        category: '',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        podcast: expect.any(Podcast),
+      };
+      const podcastId = 1;
+      jest.spyOn(service, 'getEpisodes').mockResolvedValue({
+        ok: true,
+        error: null,
+        episodes: [testEpisode],
+      });
+      const result = await service.getEpisode(getEpisodeArg);
+      expect(result).toHaveProperty('ok', true);
+      expect(result).toHaveProperty('episode');
+    });
+    it('should fail if not found', async () => {
+      const testEpisode: Episode = {
+        id: 5,
+        title: '',
+        category: '',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        podcast: expect.any(Podcast),
+      };
+      const podcastId = 1;
+      jest.spyOn(service, 'getEpisodes').mockResolvedValue({
+        ok: true,
+        error: null,
+        episodes: [testEpisode],
+      });
+      const result = await service.getEpisode(getEpisodeArg);
+      expect(result).toEqual({
+        ok: false,
+        error: `Episode with id ${getEpisodeArg.episodeId} not found in podcast with id ${getEpisodeArg.podcastId}`,
+      });
+    });
+  });
+  describe('createEpisode', () => {
+    const createEpisodeArg = {
+      podcastId: 1,
+      title: '',
+      category: '',
+    };
+    it('should fail when getPodcast fails', async () => {
+      jest.spyOn(service, 'getPodcast').mockResolvedValue({
+        podcast: null,
+        ok: false,
+        error: 'erred',
+      });
+      const result = await service.createEpisode(createEpisodeArg);
+      expect(result).toEqual({
+        ok: false,
+        error: 'erred',
+      });
+    });
+    it('should fail with error', async () => {
+      jest.spyOn(service, 'getPodcast').mockRejectedValue(new Error());
+      const result = await service.createEpisode(createEpisodeArg);
+      expect(result).toEqual(InternalServerErrorOutput);
+    });
+    it('should succeed', async () => {
+      const testPodcast: Podcast = {
+        id: 5,
+        title: '',
+        rating: 4,
+        category: '',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        episodes: [],
+      };
+      jest.spyOn(service, 'getPodcast').mockResolvedValue({
+        podcast: testPodcast,
+        ok: true,
+        error: null,
+      });
+      episodeRepository.create.mockReturnValue({
+        title: createEpisodeArg.title,
+        category: createEpisodeArg.category,
+        podcast: null,
+      });
+      episodeRepository.save.mockResolvedValue({
+        id: 1,
+      });
+      const result = await service.createEpisode(createEpisodeArg);
+      expect(result).toEqual({
+        ok: true,
+        id: 1,
+      });
+    });
+  });
+  describe('deleteEpisode', () => {
+    const testEpisode: Episode = {
+      id: 5,
+      title: '',
+      category: '',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      podcast: expect.any(Podcast),
+    };
+    const deleteEpisodeArgs = {
+      podcastId: 1,
+      episodeId: 2,
+    };
+    it('should fail with error', async () => {
+      jest.spyOn(service, 'getEpisode').mockRejectedValue(new Error());
+      const result = await service.deleteEpisode(deleteEpisodeArgs);
+      expect(result).toEqual(InternalServerErrorOutput);
+    });
+    it('should fail if getEpisode fails', async () => {
+      jest.spyOn(service, 'getEpisode').mockResolvedValue({
+        ok: false,
+        error: 'erred',
+        episode: null,
+      });
+      const result = await service.deleteEpisode(deleteEpisodeArgs);
+      expect(result).toEqual({ ok: false, error: 'erred' });
+    });
+    it('should succeed', async () => {
+      jest.spyOn(service, 'getEpisode').mockResolvedValue({
+        ok: true,
+        error: 'erred',
+        episode: testEpisode,
+      });
+      const result = await service.deleteEpisode(deleteEpisodeArgs);
+      expect(result).toEqual({ ok: true });
+    });
+  });
+  describe('updateEpisode', () => {
+    const testEpisode: Episode = {
+      id: 5,
+      title: '',
+      category: '',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      podcast: expect.any(Podcast),
+    };
+    const updateEpisodeArgs = {
+      podcastId: 1,
+      episodeId: 2,
+    };
+    it('should fail with error', async () => {
+      jest.spyOn(service, 'getEpisode').mockRejectedValue(new Error());
+      const result = await service.updateEpisode(updateEpisodeArgs);
+      expect(result).toEqual(InternalServerErrorOutput);
+    });
+    it('should fail if getEpisode fails', async () => {
+      jest.spyOn(service, 'getEpisode').mockResolvedValue({
+        ok: false,
+        error: 'erred',
+        episode: null,
+      });
+      const result = await service.updateEpisode(updateEpisodeArgs);
+      expect(result).toEqual({ ok: false, error: 'erred' });
+    });
+    it('should succeed', async () => {
+      jest.spyOn(service, 'getEpisode').mockResolvedValue({
+        ok: true,
+        error: 'erred',
+        episode: testEpisode,
+      });
+      const result = await service.updateEpisode(updateEpisodeArgs);
+      expect(result).toEqual({ ok: true });
+    });
+  });
 });
